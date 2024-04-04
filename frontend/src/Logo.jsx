6244873dfaf05776997ea10b3a0c5fbf4ab14f4d
Jackie Fang <jackiefang98@hotmail.com>
2024-04-05 02:32:02 +1300

    new logo

--- frontend/src/Logo.jsx
+++ frontend/src/Logo.jsx
@@ -1,26 +1,17 @@
-
 import './Logo.css';
 
-
-
-
-
-
-
-
-const Logo =() => {
-
-
-
-
+const Logo = () => {
     return (
         <div className="Logo">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/f/fb/Avatar_Logo_4c.svg" alt="Description of the image" />
-
+            <div className="left-image">
+                <img src="src/assets/images/Avatar_Logo.svg" alt="Avatar Logo" style={{ width: '200px' }} />
+            </div>
+            <h1 style={{ fontSize: '100px', color: 'black', gridColumn: '2 / span 1', whiteSpace: 'nowrap' }}>Avatar Universe Hub</h1>
+            <div className="right-image">
+                <img src="src/assets/images/Legend_of_Korra_logo.png" alt="Legend of Korra Logo" style={{ width: '200px' }} />
+            </div>
         </div>
-      );
+    );
 }
 
 export default Logo;
-
-
